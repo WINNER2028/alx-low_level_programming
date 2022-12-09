ee31b9e609771e7906d2aa0fa034313ae9a2924f
WINNER2028 <obiwinner94@gmail.com>
2022-12-09 15:11:54 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,18 @@
 
 /**
  * main - print the letter of the alphabet
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i = 97;
+
 	while (i < 123)
 	{
 		putchar(i);
 		i++;
 	}
-	putchar(0);
+	putchar(10);
+
 	return (0);
 }
