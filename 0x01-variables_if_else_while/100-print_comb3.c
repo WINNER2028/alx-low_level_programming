67493f00cf52f104fbf94f56538a728c45db2671
WINNER2028 <obiwinner94@gmail.com>
2022-12-09 14:09:44 -0800

    advanced tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - print double digit combos
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+
+		i++;
+	}
+
+	putchar(0);
+
+	return (0);
+}
