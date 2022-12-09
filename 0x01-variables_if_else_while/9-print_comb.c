4871930a36bd06522647158ae140eb5794ec2034
WINNER2028 <obiwinner94@gmail.com>
2022-12-09 13:12:17 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i !=57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
