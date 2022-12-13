d819a1b5f511a3162785d379f7fae636d1c1fa65
WINNER2028 <obiwinner94@gmail.com>
2022-12-13 01:37:55 -0800

    6 to 11 tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers from input
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: Always (0)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
