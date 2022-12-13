5e5d52552d392ca25077eab6cf5346baed36d37e
WINNER2028 <obiwinner94@gmail.com>
2022-12-13 08:13:46 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,6 @@
 void times_table(void)
 {
 	int a, b, c;
-	
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -18,11 +17,10 @@ void times_table(void)
 				{
 					_putchar('0');
 				}
-				
 				if (b != 0)
 				{
 					_putchar(' ');
-					_putchar((c % 10) +'0');
+					_putchar((c % 10) + '0');
 				}
 				if (b < 9)
 				{
@@ -42,7 +40,7 @@ void times_table(void)
 				}
 			}
 		}
-		
+
 		_putchar('\n');
 	}
 }
