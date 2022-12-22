5bd23425211aed8e0febea98752455cc85fe28cf
WINNER2028 <obiwinner94@gmail.com>
2022-12-22 10:12:13 -0800

    more magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,10 +3,11 @@
 #include <stdio.h>
 
 /**
-  *void prototypes
-  *int prototypes
-  *char prototypes
+  *void prototypes()
+  *int prototypes()
+  *char prototypes()
   */
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
