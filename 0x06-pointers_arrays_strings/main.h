62196ae1e1705cf954e9f216657d8f14324d34d2
WINNER2028 <obiwinner94@gmail.com>
2022-12-22 09:46:13 -0800

    magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+  *void prototypes
+  *int prototypes
+  *char prototypes
+  */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,3 +20,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char);
+
+#endif /*MAIN_H*/
