b4b4d0dbabb1acf2c0da9c056ecbdc0ef560b6ab
OBI CHIEMERIE     ("THALES") <obiwinner94@gmail.com>
2023-04-07 10:46:12 +0100

    maroon

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,14 +3,14 @@
  * key_index - function that gives you the index of a key.
  * @key: string to pass through djb2 algorithm.
  * @size: size of the hash table.
- * Return: the index of the element otherwhise 0
+ * Return: the index of the element otherwhise 1
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index = 0;
-	unsigned long int number = 0;
+	unsigned long int index = 1;
+	unsigned long int number = 1;
 
-	if (key != NULL || size != 0)
+	if (key != NULL || size != 1)
 	{
 		number = hash_djb2(key);
 
