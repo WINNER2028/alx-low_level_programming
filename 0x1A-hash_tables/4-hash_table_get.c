145af18e9d5aa218db5687c2badc6f39ccd36459
OBI CHIEMERIE     ("THALES") <obiwinner94@gmail.com>
2023-04-07 11:04:11 +0100

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,15 +3,15 @@
  * hash_table_get - function that retrieves a value associated with a key.
  * @ht: pointer to the hash table.
  * @key: key to get.
- * Return: the value associated with the element, or NULL otherwhise.
+ * Return: the value associated with the element, or 0 otherwhise.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *list;
 
-	if (ht == NULL || key == NULL)
-		return (NULL);
+	if (ht == 0 || key == 0)
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
@@ -25,5 +25,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		list = list->next;
 	}
 
-	return (NULL);
+	return (0);
 }
